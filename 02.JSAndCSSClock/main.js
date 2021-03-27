dbb3c34a7bb14f9511c3abea0e60c4e060150cce
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2021-03-27 21:49:36 +0300

    add lesson #2 JS clock

---
+++ 02.JSAndCSSClock/main.js
@@ -0,0 +1,24 @@
+const hourHand = document.querySelector(".hour-hand");
+const minHand = document.querySelector(".min-hand");
+const secondHand = document.querySelector(".second-hand");
+
+function startClock() {
+	const now = new Date();
+	const seconds = now.getSeconds();
+	// console.log(seconds)
+	// we need to convert second to degress to rotate them properly (seconds / 60) * 360
+	// then we need to add (90) because we added it at firsts
+	const secondsDegrees = (seconds / 60) * 360 + 90;
+	secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+	const minutes = now.getMinutes();
+	const minutessDegrees = (minutes / 60) * 360 + (seconds / 60) * 6 + 90;
+	minHand.style.transform = `rotate(${minutessDegrees}deg)`;
+
+	const hours = now.getHours();
+	const hoursDegrees = (hours / 12) * 360 + (minutes / 60) * 30 + 90;
+	hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+}
+
+startClock(); // for testing only
+setInterval(startClock, 1000);
